03124aa8b8faa29c3c3a1b35344c174ed14bc0cd
KLEYER <38478356+RRINKIN@users.noreply.github.com>
2022-04-14 22:46:42 +0200

    Delete main.js
    removing the JS file as no Javascript is required in this template so far.

--- js/main.js
+++
@@ -1,9 +0,0 @@
-let menuMobileDom = document.getElementById("menuHam");
-let menuDom = document.getElementById("mainNav");
-
-if(menuMobileDom != null && menuDom != null)
-{
-    menuMobileDom.addEventListener('click', function(){
-        menuDom.classList.toggle("is-active");
-    });
-}
